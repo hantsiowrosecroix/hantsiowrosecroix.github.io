35658cacfe257ffa30339037bff517dcaed0d7ef
Marcel <52205196+marcel-dev-acc@users.noreply.github.com>
2026-07-04 21:38:23 +0100

    fix: update Cloudflare proxy URL for production environment

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,2 +1,2 @@
-// export const CLOUDFLARE_PROXY_URL = 'https://rose-croix-cloudflare-proxy.hantsiowrosecroix.workers.dev/';
-export const CLOUDFLARE_PROXY_URL = 'http://localhost:8787/';
+export const CLOUDFLARE_PROXY_URL = 'https://rose-croix-cloudflare-proxy.hantsiowrosecroix.workers.dev/';
+// export const CLOUDFLARE_PROXY_URL = 'http://localhost:8787/';
